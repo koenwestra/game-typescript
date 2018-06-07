2611131b6b6473f46bd67d449702633e3871116f
Koen Westra <koenwestra>
2018-06-07 11:23:27 +0200

    changed colors

--- docs/js/snake.js
+++ docs/js/snake.js
@@ -1,66 +1,60 @@
 "use strict";
 var Game;
 (function (Game) {
-    var FLOOR = {
+    const FLOOR = {
         SPACE: 'space',
         BODY: 'body',
         FOOD: 'food'
     };
-    var Model = (function () {
-        function Model(blocks, row, col) {
+    class Model {
+        constructor(blocks, row, col) {
             this.blocks = blocks;
             this.row = row;
             this.col = col;
             this.offsets = [[-1, 0], [0, -1], [+1, 0], [0, +1]];
         }
-        Object.defineProperty(Model.prototype, "all", {
-            get: function () {
-                return this.blocks;
-            },
-            enumerable: true,
-            configurable: true
-        });
-        Model.prototype.getBlock = function (pos) {
-            return this.blocks.filter(function (block) {
+        get all() {
+            return this.blocks;
+        }
+        getBlock(pos) {
+            return this.blocks.filter((block) => {
                 if (block.pos.x === pos.x && block.pos.y === pos.y) {
                     return true;
                 }
             })[0];
-        };
-        Model.prototype.sbling = function (source, direction) {
-            var _this = this;
-            return this.blocks.filter(function (block) {
-                if (source.pos.x + _this.offsets[direction][0] === block.pos.x
-                    && source.pos.y + _this.offsets[direction][1] === block.pos.y) {
+        }
+        sbling(source, direction) {
+            return this.blocks.filter((block) => {
+                if (source.pos.x + this.offsets[direction][0] === block.pos.x
+                    && source.pos.y + this.offsets[direction][1] === block.pos.y) {
                     return true;
                 }
             })[0];
-        };
-        Model.prototype.random = function () {
-            var pos = {
+        }
+        random() {
+            let pos = {
                 x: Math.floor(Math.random() * this.col),
                 y: Math.floor(Math.random() * this.row)
             };
-            var block = this.getBlock(pos);
+            let block = this.getBlock(pos);
             return block;
-        };
-        Model.prototype.genFood = function () {
-            var block = this.random();
+        }
+        genFood() {
+            let block = this.random();
             if (block.type === FLOOR.BODY) {
                 block = this.random();
             }
             block.type = FLOOR.FOOD;
             this.render([block]);
-        };
-        Model.prototype.render = function (blocks) {
-            blocks.forEach(function (block) {
+        }
+        render(blocks) {
+            blocks.forEach(block => {
                 block.node.className = block.type;
             });
-        };
-        return Model;
-    }());
-    var Floor = (function () {
-        function Floor(options) {
+        }
+    }
+    class Floor {
+        constructor(options) {
             options = options || {};
             this.table = document.createElement('table');
             this.table.cellPadding = '0';
@@ -70,20 +64,16 @@ var Game;
             this.col = options.col || 20;
             this.blocks = [];
         }
-        Object.defineProperty(Floor.prototype, "model", {
-            get: function () {
-                return new Model(this.blocks, this.row, this.col);
-            },
-            enumerable: true,
-            configurable: true
-        });
-        Floor.prototype.initialize = function () {
-            var y;
-            var x;
+        get model() {
+            return new Model(this.blocks, this.row, this.col);
+        }
+        initialize() {
+            let y;
+            let x;
             for (y = 0; y < this.row; y++) {
-                var tr = this.table.insertRow(-1);
+                let tr = this.table.insertRow(-1);
                 for (x = 0; x < this.col; x++) {
-                    var td = tr.insertCell(-1);
+                    let td = tr.insertCell(-1);
                     td.className = FLOOR.SPACE;
                     this.blocks.push({
                         node: td,
@@ -93,12 +83,11 @@ var Game;
                 }
             }
             this.parent.appendChild(this.table);
-        };
-        return Floor;
-    }());
+        }
+    }
     Game.Floor = Floor;
-    var Snake = (function () {
-        function Snake(floor, options) {
+    class Snake {
+        constructor(floor, options) {
             options = options || {};
             this.model = floor.model;
             this.initLength = options.initLength || 3;
@@ -109,66 +98,65 @@ var Game;
             this.score = 0;
             this.step = 0;
         }
-        Snake.prototype.born = function () {
-            var _this = this;
+        born() {
             var lastStep;
             var lastKeyCode;
             var setDirectionTimer;
-            var setDirection = function (keyCode) {
+            let setDirection = (keyCode) => {
                 switch (keyCode) {
                     case 37:
-                        if (_this.direction !== 2) {
-                            _this.direction = 0;
+                        if (this.direction !== 2) {
+                            this.direction = 0;
                         }
                         break;
                     case 38:
-                        if (_this.direction !== 3) {
-                            _this.direction = 1;
+                        if (this.direction !== 3) {
+                            this.direction = 1;
                         }
                         break;
                     case 39:
-                        if (_this.direction !== 0) {
-                            _this.direction = 2;
+                        if (this.direction !== 0) {
+                            this.direction = 2;
                         }
                         break;
                     case 40:
-                        if (_this.direction !== 1) {
-                            _this.direction = 3;
+                        if (this.direction !== 1) {
+                            this.direction = 3;
                         }
                         break;
                 }
             };
-            var keyHandler = function (e) {
-                var keyCode = e.keyCode || e.which || e.charCode;
+            let keyHandler = (e) => {
+                const keyCode = e.keyCode || e.which || e.charCode;
                 e.preventDefault();
                 if (setDirectionTimer) {
                     clearTimeout(setDirectionTimer);
                 }
-                if (_this.step === lastStep) {
+                if (this.step === lastStep) {
                     lastKeyCode = keyCode;
                     setDirectionTimer = setTimeout(function () {
                         setDirection(lastKeyCode);
-                    }.bind(_this), _this.speed);
+                    }.bind(this), this.speed);
                     return;
                 }
                 setDirection(keyCode);
-                lastStep = _this.step;
+                lastStep = this.step;
             };
             document.addEventListener('keydown', keyHandler, false);
-            for (var i = this.initLength - 1; i >= 0; i--) {
+            for (let i = this.initLength - 1; i >= 0; i--) {
                 this.bodies.push(this.model.all[i]);
             }
-            this.bodies.forEach(function (body) {
+            this.bodies.forEach(body => {
                 body.type = FLOOR.BODY;
             });
             this.model.render(this.bodies);
             this.model.genFood();
             this.timer = setInterval(function () { this.move(); }.bind(this), this.speed);
-        };
-        Snake.prototype.move = function () {
-            var head = this.bodies[0];
-            var tail = this.bodies[this.bodies.length - 1];
-            var next = this.model.sbling(head, this.direction);
+        }
+        move() {
+            let head = this.bodies[0];
+            let tail = this.bodies[this.bodies.length - 1];
+            let next = this.model.sbling(head, this.direction);
             if (!next || next.type === FLOOR.BODY) {
                 this.die();
                 return;
@@ -176,7 +164,7 @@ var Game;
             if (next.type === FLOOR.FOOD) {
                 this.eat(next);
             }
-            for (var i = this.bodies.length - 1; i > 0; i--) {
+            for (let i = this.bodies.length - 1; i > 0; i--) {
                 this.bodies[i] = this.bodies[i - 1];
             }
             next.type = FLOOR.BODY;
@@ -185,35 +173,34 @@ var Game;
             this.model.render([tail]);
             this.model.render(this.bodies);
             this.step++;
-        };
-        Snake.prototype.die = function () {
+        }
+        die() {
             clearInterval(this.timer);
             alert('Game Over!');
-        };
-        Snake.prototype.eat = function (block) {
+        }
+        eat(block) {
             this.bodies.push(block);
             this.model.genFood();
             this.score++;
-        };
-        return Snake;
-    }());
+        }
+    }
     Game.Snake = Snake;
 })(Game || (Game = {}));
 var Game;
 (function (Game) {
-    var start = document.getElementById('start');
-    var score = document.getElementById('score');
-    var floor = new Game.Floor({
+    const start = document.getElementById('start');
+    const score = document.getElementById('score');
+    const floor = new Game.Floor({
         parent: document.getElementById('container')
     });
     floor.initialize();
-    var snake = new Game.Snake(floor);
+    const snake = new Game.Snake(floor);
     start.onclick = function () {
         snake.born();
         this.setAttribute('disabled', true);
     };
-    var observer = function (changes) {
-        changes.forEach(function (change) {
+    const observer = (changes) => {
+        changes.forEach((change) => {
             if (change.name === 'score') {
                 score.textContent = change.object[change.name];
             }
